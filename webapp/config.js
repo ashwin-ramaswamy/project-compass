e34846063cd6e5dbba27fd746e6a01fc6a150034
ashwin-ramaswamy <ashwin.ramaswamy@gmail.com>
2025-12-29 19:02:48 +0530

    Create config.js

---
+++ webapp/config.js
@@ -0,0 +1,5 @@
+const CONFIG = {
+  SHEET_URL: 'https://script.google.com/macros/s/AKfycbwHJtAnljQNiEWOpUmOf2_at2cc6D1UGixxq-va7NDUOx8jUmzeUaCgM8HAPV9nev40HA/exec',
+  BACKEND_URL: 'https://project-compass-9gls.onrender.com',
+  MODEL_DISPLAY: 'GPT-4o Mini'
+};
